f0ebe38e2e4aeee574c70a6a93d08c6867dbf335
Siddharth Mahendiran <pssiddharth436@gmail.com>
2025-11-28 18:08:31 +0530

    Folder Changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Global tokens + global defaults
+import "./styles/global.css";
+
+// Page/component styles can load afterwards
+import "./styles/SignUp.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
