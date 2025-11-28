e314f35115dd00b6ab339ea48b0502251a9907c8
Siddharth Mahendiran <pssiddharth436@gmail.com>
2025-11-28 15:17:50 +0530

    Add new SignUp component and updated styles

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  SignUp from "./Components/SignUp";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/SignUp.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles.css";
+
+export const SignUp = () => {
+  return (
+    <div className="signup-container">
+      <div className="logo">8BitLog</div>
+
+      
+      <p className="subtitle">Create an account to continue.</p>
+
+      <div className="input-box">
+        <input className="input-field" placeholder="Username" />
+      </div>
+
+      <div className="input-box">
+        <input className="input-field" placeholder="Email" />
+      </div>
+
+      <div className="input-box">
+        <input className="input-field" type="password" placeholder="Password" />
+      </div>
+
+      <button className="signup-btn">Sign Up</button>
+
+      <p className="login-text">
+        Already have an account? <span className="login-link">Login Page</span>.
+      </p>
+    </div>
+  );
+};
+export default SignUp;
